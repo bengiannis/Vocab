4b450672c30a5a7ce3c6f1d0ef515e989a57fa50
Fouad <fouad99877@icloud.com>
2022-03-31 23:53:19 -0400

    2 UI Screens have been added also the button("Start Button") is functional and will navigates you to the second UI Screen

--- app/src/main/java/com/javageeks/vocab/MainActivity.java
+++ app/src/main/java/com/javageeks/vocab/MainActivity.java
@@ -2,13 +2,25 @@ package com.javageeks.vocab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+Button StartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StartButton=(Button)findViewById(R.id.StartButton);
+        StartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =  new Intent(MainActivity.this,SecondScreen.class);
+                startActivity(i);
+            }
+        });
     }
 }
