f893d7b7dac4ef9e9f12cf6de2c973d066b51d46
Fouad <fouad99877@icloud.com>
2022-04-02 00:34:30 -0400

    UI#2 images are clickable and navigates you to their own corresponding search page

---
+++ app/src/main/java/com/javageeks/vocab/SearchActivity4.java
@@ -0,0 +1,14 @@
+package com.javageeks.vocab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search4);
+    }
+}
--- app/src/main/java/com/javageeks/vocab/SecondScreen.java
+++ app/src/main/java/com/javageeks/vocab/SecondScreen.java
@@ -2,13 +2,52 @@ package com.javageeks.vocab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SecondScreen extends AppCompatActivity {
+    ImageButton imageButton;
+    ImageButton imageButton2;
+    ImageButton imageButton3;
+    ImageButton imageButton4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_screen);
+        imageButton=(ImageButton)findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent j =  new Intent(SecondScreen.this,SearchActivity.class);
+                startActivity(j);
+            }
+        });
+        imageButton2 = (ImageButton) findViewById(R.id.imageButton2);
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent k =  new Intent(SecondScreen.this,SearchActivity2.class);
+                startActivity(k);
+            }
+        });
+        imageButton3 =(ImageButton) findViewById(R.id.imageButton3);
+        imageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent l = new Intent(SecondScreen.this, SearchActivity3.class);
+                startActivity(l);
+            }
+        });
+        imageButton4 = (ImageButton)findViewById(R.id.imageButton4);
+        imageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent h = new Intent(SecondScreen.this,SearchActivity4.class);
+                startActivity(h);
+            }
+        });
     }
 }
